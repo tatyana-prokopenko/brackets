a4fd83a4088e83f2a73d5e3bfb9c78d4a348b7dd
tatyana-prokopenko <tatyana-prokopenko@bk.ru>
2025-07-16 17:18:36 +0300

    feat: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  let savedStr = str;
+  let result;
+  (function deleteBrackets() {
+    bracketsConfig.forEach(function reducesavedStr(element) {
+      const bracketIndex = savedStr.indexOf(element.join(''));
+      if (bracketIndex !== -1) {
+        savedStr =
+          savedStr.slice(0, bracketIndex) + savedStr.slice(bracketIndex + 2);
+        if (savedStr !== '') {
+          reducesavedStr(element);
+        }
+      }
+    });
+    if (savedStr === '') {
+      result = true;
+    } else if (
+      bracketsConfig
+        .map((elem) => {
+          return savedStr.indexOf(elem.join(''));
+        })
+        .some((value) => value >= 0)
+    ) {
+      deleteBrackets();
+    } else {
+      result = false;
+    }
+  })();
+  return result;
 };
